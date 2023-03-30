09aae3e176af79e54c43f576e2ab68bfc4fde1d4
roushanali064 <roushanrhaman064@gmail.com>
2023-03-30 15:28:43 +0600

    added cart compo and calculation

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,13 @@
+
 import React from 'react';
+
 import './Product.css'
 
 const Product = (props) => {
 
-    console.log(props.product)
     const { img, name, seller, ratings, price } = props.product
+
+    const handleAddToCart = props.handleAddToCart
 
     return (
         <div className='product'>
@@ -15,7 +18,11 @@ const Product = (props) => {
                 <p>Manufacturer : {seller}</p>
                 <p>Rating : {ratings} Star</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={()=> handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                
+                
+                </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
 
     const [products, setProducts] = useState([])
-    useEffect(()=>{
+    const [cart, setCart] = useState([])
+    useEffect(() => {
         fetch('products.json')
-        .then(res=> res.json())
-        .then(data=> setProducts(data))
-    },[])
-    
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    const handleAddToCart = (props) => {
+        const newCart = [...cart,props]
+        setCart(newCart)
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product=> <Product
-                    key={product.id}
-                    product={product}
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h1>Order Sumary</h1>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
